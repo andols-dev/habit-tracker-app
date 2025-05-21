a523e441098725691af2f603c6d221968353a03f
Andreas Olsson <andreasolsson2020@outlook.com>
2025-05-21 14:36:30 +0200

    update browserrouter base name

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/habit-tracker-app">
       <App />
     </BrowserRouter>
   </StrictMode>,
